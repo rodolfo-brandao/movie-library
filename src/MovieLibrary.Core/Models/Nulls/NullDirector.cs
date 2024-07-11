6922d104010acf2e1b45ae0f114bd0b70b74a9a4
Rodolfo <rodolfobrandao1995@gmail.com>
2024-07-10 21:20:41 -0300

    feat: add null objects for model entities
    - NullDirector
    - NullMovie
    - NullUser

---
+++ src/MovieLibrary.Core/Models/Nulls/NullDirector.cs
@@ -0,0 +1,7 @@
+using MovieLibrary.Core.Models.Abstract;
+
+namespace MovieLibrary.Core.Models.Nulls;
+
+public sealed class NullDirector : INullObject<Director>
+{
+}
---
+++ src/MovieLibrary.Core/Models/Nulls/NullUser.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
+using MovieLibrary.Core.Models.Abstract;
+
+namespace MovieLibrary.Core.Models.Nulls;
+
+[ExcludeFromCodeCoverage]
+public sealed class NullUser : INullObject<User>
+{
+}
