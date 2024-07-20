7627758052f1dbfac85c64ac6882569468007fe4
Rodolfo <rodolfobrandao1995@gmail.com>
2024-07-20 16:01:45 -0300

    feat: create DbContext

---
+++ src/MovieLibrary.Data/DbContexts/MovieLibraryDbContext.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using MovieLibrary.Core.Models;
+
+namespace MovieLibrary.Data.DbContexts;
+
+[ExcludeFromCodeCoverage]
+public sealed class MovieLibraryDbContext : DbContext
+{
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public MovieLibraryDbContext(DbContextOptions<MovieLibraryDbContext> options) : base(options)
+    {
+        ChangeTracker.LazyLoadingEnabled = default;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
