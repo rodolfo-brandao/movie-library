bf1d204b132edf3645e2d1c0991b3c9abbdb59e4
Rodolfo <rodolfobrandao1995@gmail.com>
2024-09-07 11:39:08 -0300

    refactor: improve validator for creating a user
    Improvements:
    - Check if the user is disabled or not in the database
    - Use the .NET built in class for managing mail addresses

--- src/MovieLibrary.Application/Commands/Users/CreateUser/CreateUserCommandValidator.cs
+++ src/MovieLibrary.Application/Commands/Users/CreateUser/CreateUserCommandValidator.cs
@@ -1,4 +1,4 @@
-using MovieLibrary.Application.Utils.Structures;
+using System.Net.Mail;
 using MovieLibrary.Core.Contracts.Repositories;
 
 namespace MovieLibrary.Application.Commands.Users.CreateUser;
@@ -10,7 +10,6 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
     private const ushort EmailMaxLength = 255;
     private const ushort PasswordMinLength = 8;
 
-
     public CreateUserCommandValidator(IUserRepository userRepository)
     {
         RuleFor(command => command.Username)
@@ -20,7 +19,7 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
             .WithMessage($"The username must have at least {UsernameMinLength} characters.")
             .MaximumLength(maximumLength: UsernameMaxLength)
             .WithMessage($"The username must not exceed {UsernameMaxLength} characters.")
-            .MustAsync(async (username, _) => !await userRepository.ExistsAsync(user => user.Username.Equals(username)))
+            .MustAsync(async (username, _) => !await userRepository.ExistsAsync(user => user.Username.Equals(username) && !user.IsDisabled))
             .WithMessage(command => $"User with username '{command.Username}' already exists.");
 
         RuleFor(command => command.Email)
@@ -28,9 +27,9 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
             .WithMessage("The e-mail cannot be empty, null or whitespace.")
             .MaximumLength(maximumLength: EmailMaxLength)
             .WithMessage($"The e-mail must not exceed {EmailMaxLength} characters.")
-            .Must(email => new EmailAddress(email).IsValid())
+            .Must(email => MailAddress.TryCreate(email, out var _))
             .WithMessage("The e-mail address is not valid.")
-            .MustAsync(async (email, _) => !await userRepository.ExistsAsync(user => user.Email.Equals(email)))
+            .MustAsync(async (email, _) => !await userRepository.ExistsAsync(user => user.Email.Equals(email) && !user.IsDisabled))
             .WithMessage(command => $"User with e-mail '{command.Email}' already exists.");
 
         RuleFor(command => command.Password)
